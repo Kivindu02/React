b5a88714525756313cbdbc824c85a11b4e9cad0a
Kivindu <manusha.rajamanukula@gmail.com>
2025-12-26 02:00:34 +0530

    Change the icons in the tabs

--- ecommerce-project/src/pages/checkout/CheckoutPage.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -6,6 +6,7 @@ export function CheckoutPage() {
   return (
 
     <>
+      <link rel="icon" type="image/svg+xml" href="/cart-favicon.png" />
       <title>Checkout</title>
 
       <CheckoutHeader />
