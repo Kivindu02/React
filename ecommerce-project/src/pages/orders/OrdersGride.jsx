6805845afb08e51cac38390b7a6bbf69cce45b7e
Kivindu <manusha.rajamanukula@gmail.com>
2025-12-31 19:20:30 +0530

    Generate the HTML for the OrdersGrid, OrderHeader and OrderDetailsGrid

---
+++ ecommerce-project/src/pages/orders/OrdersGride.jsx
@@ -0,0 +1,25 @@
+import { OrderHeader } from "./OrderHeader";
+import { OrderDetailsGrid } from "./OrderDetailsGrid";
+
+
+export function OrdersGride({ orders }) {
+  return (
+    <div className="orders-grid">
+      {orders.map((order) => {
+        return (
+
+          <div key={order.id} className="order-container">
+
+            <OrderHeader order={order} />
+
+            <OrderDetailsGrid order={order}/>
+          </div>
+
+        );
+
+      })}
+
+    </div>
+  );
+
+}
