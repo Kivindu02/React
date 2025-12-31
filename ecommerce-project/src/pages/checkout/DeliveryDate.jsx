c06e1dd9bacf332e46849db9de7c0647f68b1a96
Kivindu <manusha.rajamanukula@gmail.com>
2025-12-31 18:04:18 +0530

    Convert order async await and sperate the  HTML for CartItemDetails and DeliveryDate

---
+++ ecommerce-project/src/pages/checkout/DeliveryDate.jsx
@@ -0,0 +1,15 @@
+import dayjs from "dayjs";
+
+export function DeliveryDate({ cartItem, deliveryOptions }) {
+
+  const selectDeliveryOption = deliveryOptions.find((deliveryOption) => {
+    return deliveryOption.id === cartItem.deliveryOptionId;
+  });
+
+  return (
+    <div className="delivery-date">
+      Delivery date: {dayjs(selectDeliveryOption.estimatedDeliveryTimeMs).format('dddd, MMMM D')}
+    </div>
+  );
+
+}
