88563362dbb677c98567c82a00373638300f6cfb
Kivindu <manusha.rajamanukula@gmail.com>
2026-01-01 14:39:29 +0530

    Use axios to fetch the data for TrackingPage

--- ecommerce-project/src/pages/TrackingPage.jsx
+++ ecommerce-project/src/pages/TrackingPage.jsx
@@ -1,15 +1,38 @@
-import { Link } from 'react-router';
+import { Link, useParams } from 'react-router';
 import { Header } from '../components/Header';
 import './TrackingPage.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import dayjs from 'dayjs';
 
-export function TrackingPage() {
+export function TrackingPage({ cart }) {
+
+  const {orderId, productId} = useParams();
+  const [order, setOrder] = useState(null);
+  
+  useEffect(()=>{
+    const fetcTrakingData = async () => {
+      const response = await axios.get(`/api/orders/${orderId}?expand=products`);
+      setOrder(response.data);
+    }
+    fetcTrakingData();
+  },[orderId]);
+
+  if(!order){
+    return null;
+  }
+
+  const orderProduct = order.products.find((orderProduct)=>{
+    return orderProduct.productId === productId;
+  });
+
   return (
     <>
       <link rel="icon" type="image/svg+xml" href="/tracking-favicon.png" />
 
       <title>Tracking</title>
 
-      <Header />
+      <Header cart={cart} />
 
       <div class="tracking-page">
         <div class="order-tracking">
@@ -18,18 +41,18 @@ export function TrackingPage() {
           </Link>
 
           <div class="delivery-date">
-            Arriving on Monday, June 13
+            {dayjs(orderProduct.estimatedDeliveryTimeMs).format('dddd, MMMM D')}
           </div>
 
           <div class="product-info">
-            Black and Gray Athletic Cotton Socks - 6 Pairs
+            {orderProduct.product.name}
           </div>
 
           <div class="product-info">
-            Quantity: 1
+            Quantity: {orderProduct.quantity}
           </div>
 
-          <img class="product-image" src="images/products/athletic-cotton-socks-6-pairs.jpg" />
+          <img class="product-image" src={orderProduct.product.image} />
 
           <div class="progress-labels-container">
             <div class="progress-label">
--- ecommerce-project/src/pages/checkout/CheckoutHeader.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutHeader.jsx
@@ -4,26 +4,33 @@ import Logo from '../../assets/images/logo.png';
 import MobileLogo from '../../assets/images/mobile-logo.png';
 import './CheckoutHeader.css';
 
-export function CheckoutHeader(){
-  return(
+export function CheckoutHeader({ cart }) {
+
+  let totalQuantity = 0;
+
+  cart.forEach((cartItem) => {
+    totalQuantity += cartItem.quantity;
+    })
+
+  return (
     <div className="checkout-header">
-        <div className="header-content">
-          <div className="checkout-header-left-section">
-            <Link to="/">
-              <img className="logo" src={Logo} />
-              <img className="mobile-logo" src={MobileLogo} />
-            </Link>
-          </div>
+      <div className="header-content">
+        <div className="checkout-header-left-section">
+          <Link to="/">
+            <img className="logo" src={Logo} />
+            <img className="mobile-logo" src={MobileLogo} />
+          </Link>
+        </div>
 
-          <div className="checkout-header-middle-section">
-            Checkout (<Link className="return-to-home-link"
-              to="/">3 items</Link>)
-          </div>
+        <div className="checkout-header-middle-section">
+          Checkout (<Link className="return-to-home-link"
+            to="/">{totalQuantity} items</Link>)
+        </div>
 
-          <div className="checkout-header-right-section">
-            <img src={CheckoutLockIcon} />
-          </div>
+        <div className="checkout-header-right-section">
+          <img src={CheckoutLockIcon} />
         </div>
       </div>
+    </div>
   );
 }
